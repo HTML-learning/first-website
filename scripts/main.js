5c23e5f358c1ce1185a69a1d2cccfa47745f0d7e
Jack YANG <pptlys@gmail.com>
2021-02-28 14:58:33 -0500

    refactor js

--- scripts/main.js
+++ scripts/main.js
@@ -27,7 +27,7 @@ if (!localStorage.getItem('name')) {
     setUserName();
 } else {
     let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
 }
 
 myButton.onclick = function(){
